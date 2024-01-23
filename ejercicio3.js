6a8841b44ebff4507f66c472175dde4c96892b1e
Andrés Gómez <jose751a12@gmail.com>
2024-01-23 00:39:49 -0600

    primer commit

---
+++ ejercicio3.js
@@ -0,0 +1,30 @@
+var valor1 = (4)
+var valor2 = (7)
+var valor3 = (8)
+var valor4 = (9)
+var valor6 = (3)
+var valor5 = (5)
+if (valor1>valor5){
+   alert("5 es menor que 4") 
+}else{
+    alert("5 es mayor que 4")
+}
+if (valor2>valor5){
+
+alert ("5 es menor que 7")
+}else {
+    alert("5 es mayor que 7")
+}
+if (valor3>valor5){
+    alert("5 es menor que 8")
+}else[
+    alert("5 es mayor que 8")
+]
+if (valor4>valor5){
+    alert("5 es menor que 9")
+}else{"5 mayor que 9"}
+if (valor6>valor5){
+    alert("5 es menor que 3")
+}else{
+    alert ("5 es mayor que 3")
+}
